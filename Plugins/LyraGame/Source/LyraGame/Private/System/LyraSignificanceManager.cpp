ec5d47e75ce7a17d482cc645df2f51c7e33b5aae
Matt <matthis.letexier@gmail.com>
2026-03-24 19:41:19 +0100

    Update to 5.7.4 with dependencies plugin added directly

--- Plugins/LyraGame/Source/LyraGame/Private/System/LyraSignificanceManager.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "System/LyraSignificanceManager.h"
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(LyraSignificanceManager)
-
---
+++ Source/LyraGame/Teams/LyraTeamPrivateInfo.h
@@ -0,0 +1,18 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "LyraTeamInfoBase.h"
+
+#include "LyraTeamPrivateInfo.generated.h"
+
+class UObject;
+
+UCLASS()
+class LYRAGAME_API ALyraTeamPrivateInfo : public ALyraTeamInfoBase
+{
+	GENERATED_BODY()
+
+public:
+	ALyraTeamPrivateInfo(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
